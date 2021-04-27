e486d77bb748a99596ac206874250b613ed9af06
Dorra Amri <amridorrasimplonenda@gmail.com>
2021-04-27 14:16:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavigationBar from './components/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
